5617711f35941f165834cbae2f24f14f92c4eccd
chamikathereal <118957556+chamikathereal@users.noreply.github.com>
2026-01-26 15:05:10 +0530

    Initial project structure setup for nexus-inventory-suite.

---
+++ nexus-backend/src/test/java/io/github/chamikathereal/nexus_backend/NexusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.chamikathereal.nexus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
